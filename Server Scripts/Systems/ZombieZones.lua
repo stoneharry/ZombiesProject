e6bd951eb1a002acae304aaf50799d98132682d4
Stoneharry <stoneharryjames@gmail.com>
2015-06-03 01:24:18 +0100

    Updates

--- Server Scripts/Systems/ZombieZones.lua
+++ Server Scripts/Systems/ZombieZones.lua
@@ -1,16 +1,24 @@
+--[[
+SPELLS:
+90005 - Sanctuary "Argent Controlled Zone"
+90006 - Scourge "Scourge Controlled Zone"
 
-local infectedZones = { 12 } -- 12 = Elwynn Forest
+--]]
 
+local infectedZones = { [12] = true } -- 12 = Elwynn Forest
+local uninfectedAreas = { 
+	[9] = true, [24] = true, [34] = true, [59] = true --[[9 = Northshire Valley, 24 = Northshire Abbey, 34 = Northishire Mines, 59 = Northshire Vineyards ]]
+	}
 local function PLAYER_EVENT_ON_UPDATE_ZONE(event, player, newZone, newArea)
-	for _,v in pairs(infectedZones) do
-		if v == newZone then
-			player:RemoveAura(90005)
-			player:CastSpell(player, 90006, true)
-			return
+	if (infectedZones[newZone]) then
+		if not(uninfectedAreas[newArea]) then
+			player:RemoveAura(90005) -- Remove Sanctuary
+			player:CastSpell(player, 90006, true) -- Add Scourge
+			return --Stop here
 		end
 	end
-	player:RemoveAura(90006)
-	player:CastSpell(player, 90005, true)
+	player:RemoveAura(90006) -- Remove Scourge
+	player:CastSpell(player, 90005, true) -- Add Sanctuary
 end
 
 RegisterPlayerEvent(27, PLAYER_EVENT_ON_UPDATE_ZONE)
